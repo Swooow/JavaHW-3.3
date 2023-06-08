9a84437176898bcba75e41740a69e27c01477975
Swow <stepan2235@gmail.com>
2023-06-08 23:35:52 +0300

    Change commit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,5 +1,5 @@
 public class CreditPaymentService {
-    public int calculate(int credit, int month, double percent){
+    public int calculate(int credit, int month, double percent) {
         //s = k * (m × (1 + m)ⁿ) / ((1 + m)ⁿ — 1)
         // где S - аннуитетный платеж, К - полная сумма кредита,
         // m — процентная ставка в месяц, равная годовой ставке, поделённой на 12, а n — количество платежей.
@@ -9,9 +9,9 @@ public class CreditPaymentService {
         // Оператор double у = Math.pow(x, a);
         // присваивает переменной у значение переменной х, возведенное в степень а.
 
-        double m = percent/month;
-        double d = Math.pow((1 + m),month);
-        double annuity = ((m * d)/(d-1));
+        double m = percent / 12;
+        double d = Math.pow((1 + m), month);
+        double annuity = ((m * d) / (d - 1));
         double annuityPayment = credit * annuity;
 
         return (int) annuityPayment;
--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
         int credit = 1_000_000;
-        int month = 12;
+        int month = 36;
         double percent = 0.0999;
-        double annuityPayment = service.calculate(credit,month,percent);
+        double annuityPayment = service.calculate(credit, month, percent);
         System.out.println(annuityPayment);
 
     }
